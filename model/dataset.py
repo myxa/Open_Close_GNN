4606a0b875a222d4609773290d0241827a5f77f6
myxa <33979138+myxa@users.noreply.github.com>
2022-12-13 15:20:20 +0300

    initial

---
+++ model/dataset.py
@@ -0,0 +1,118 @@
+import torch
+from torch_geometric.data import Dataset, Data
+from torch_geometric.utils import dense_to_sparse
+from scipy.sparse import coo_matrix
+import numpy as np
+import os
+
+
+class OpenCloseDataset(Dataset):
+
+    def __init__(self, datafolder, test=False, transform=None, pre_transform=None):
+
+        self.test = test
+        self.datafolder = datafolder
+        self.open = np.load(f'{datafolder}/open.npy')
+        self.close = np.load(f'{datafolder}/close.npy')
+
+        super().__init__(root=datafolder, transform=transform, pre_transform=pre_transform)
+
+    @property
+    def raw_file_names(self):
+        return ['close.npy', 'open.npy']
+
+    @property
+    def processed_file_names(self):
+
+        """ If these files are found in raw_dir, processing is skipped"""
+        # todo test filenames
+        if self.test:
+            return [os.path.join(self.datafolder, 'test', f'data_{i}.pt') for i in range(47+47)]
+
+        else:
+            return [os.path.join(self.datafolder, f'data_{i}.pt') for i in range(47+47)]
+
+    def download(self):
+        # todo download data
+        pass
+
+    def process(self):
+
+        for index, matr in enumerate(self.open):
+            _ = self._load_and_save(matr, index, 'open')
+
+        for index, matr in enumerate(self.close):
+            _ = self._load_and_save(matr, index, 'close')
+
+    def _load_and_save(self, matr, index, state):
+
+        x = torch.from_numpy(matr).float()
+
+        adj = self.compute_KNN_graph(matr)
+        adj = torch.from_numpy(adj).float()
+
+        edge_index, edge_attr = dense_to_sparse(adj)
+        label = torch.tensor(0 if state == 'close' else 1).long()
+
+        data = Data(x=x, edge_index=edge_index,
+                    edge_attr=edge_attr, y=label)
+
+        index = index + 47 if state == 'close' else index
+        if self.test:
+            torch.save(data,
+                       os.path.join(self.processed_dir, 'test',
+                                    f'data_{index}.pt'))
+        else:
+            torch.save(data,
+                       os.path.join(self.processed_dir,
+                                    f'data_{index}.pt'))
+        return data
+
+    def compute_KNN_graph(self, matrix, k_degree=10):
+        """ Calculate the adjacency matrix from the connectivity matrix."""
+
+        matrix = np.abs(matrix)
+        idx = np.argsort(-matrix)[:, 0:k_degree]
+        matrix.sort()
+        matrix = matrix[:, ::-1]
+        matrix = matrix[:, 0:k_degree]
+
+        A = self._adjacency(matrix, idx).astype(np.float32)
+
+        return A
+
+    def _adjacency(self, dist, idx):
+
+        m, k = dist.shape
+        assert m, k == idx.shape
+        assert dist.min() >= 0
+
+        # Weight matrix.
+        I = np.arange(0, m).repeat(k)
+        J = idx.reshape(m * k)
+        V = dist.reshape(m * k)
+        W = coo_matrix((V, (I, J)), shape=(m, m))
+
+        # No self-connections.
+        W.setdiag(0)
+
+        # Non-directed graph.
+        bigger = W.T > W
+        W = W - W.multiply(bigger) + W.T.multiply(bigger)
+
+        return W.todense()
+
+    def len(self):
+        return 47+47  # len(self.files)
+
+    def get(self, idx):
+        """ - Equivalent to __getitem__ in pytorch
+            - Is not needed for PyG's InMemoryDataset
+        """
+        if self.test:
+            data = torch.load(os.path.join(self.processed_dir,
+                                           f'data_test_{idx}.pt'))
+        else:
+            data = torch.load(os.path.join(self.processed_dir,
+                                           f'data_{idx}.pt'))
+        return data
---
+++ model/utils.py
@@ -0,0 +1,80 @@
+import torch
+from tqdm import tqdm
+import numpy as np
+from torch_geometric.loader import DataLoader
+from sklearn.metrics import confusion_matrix
+import torch.nn.functional as F
+
+
+def fit_epoch(model, loader, criterion, optimizer):
+    model.train()
+    loss_all = 0
+    for data in loader:
+        data = data.to(device())
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, data.y)
+        loss.backward()
+        loss_all += loss.item() # data.num_graphs *
+        optimizer.step()
+    return np.mean(loss_all)
+
+
+def eval_epoch(model, loader, criterion):
+    model.eval()
+    pred = []
+    label = []
+    loss_all = 0
+    for data in loader:
+        data = data.to(device())
+        output = model(data)
+        loss = criterion(output, data.y)
+        loss_all += loss.item() # data.num_graphs *
+        pred.append(F.softmax(output, dim=1).max(dim=1)[1])
+        label.append(data.y)
+
+    y_pred = torch.cat(pred, dim=0).cpu().detach().numpy()
+    y_true = torch.cat(label, dim=0).cpu().detach().numpy()
+    tn, fp, fn, tp = confusion_matrix(y_pred, y_true).ravel()
+    # print(tn)
+    epoch_sen = tp / (tp + fn + 0.000001)
+    epoch_spe = tn / (tn + fp + 0.000001)
+    epoch_acc = (tn + tp) / (tn + tp + fn + fp)
+    return epoch_sen, epoch_spe, epoch_acc, np.mean(loss_all)
+
+
+def device():
+    return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def train(train_dataset, val_dataset, model, epochs, batch_size, opt, criterion, save_best=False):
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+
+    history = []
+    best_val_loss = 1000
+    log_template = "train_loss {t_loss:0.4f} " \
+                   "val_loss {v_loss:0.4f} val_acc {v_acc:0.4f} " \
+                   "\nprecision {precision:0.3f} recall {recall:0.3f}"
+    with tqdm(desc="epoch", total=epochs) as pbar_outer:
+
+        for epoch in range(1, epochs+1):
+            train_loss = fit_epoch(model, train_loader, criterion, opt)
+            precision, recall, acc, val_loss = eval_epoch(model, val_loader, criterion)
+            history.append((train_loss, val_loss, acc, precision, recall))
+
+            if save_best:
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    torch.save(model.state_dict(), save_best)
+
+            pbar_outer.update(1)
+            tqdm.write(log_template.format(t_loss=train_loss,
+                                           v_loss=val_loss, v_acc=acc,
+                                           precision=precision, recall=recall))
+            print('')
+
+    if save_best:
+        model.load_state_dict(torch.load(save_best, map_location=device()))
+
+    return history
