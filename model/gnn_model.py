f45766506505f007ac0598aacc967cbb63d8ee99
myxa <33979138+myxa@users.noreply.github.com>
2023-02-13 19:05:37 +0300

    gat added

--- model/gnn_model.py
+++ model/gnn_model.py
@@ -1,8 +1,9 @@
 import torch.nn.functional as F
 import torch.nn as nn
-from torch_geometric.nn import global_mean_pool, GCNConv, GATConv
+from torch_geometric.nn import global_mean_pool, GCNConv, GATv2Conv, GATConv
 
 # todo сделать module list
+
 
 class GCN(nn.Module):
     def __init__(self,
@@ -56,4 +57,23 @@ class GCN(nn.Module):
             x = self.lin1(x)
             return x
 
+
+class GATv2(nn.Module):
+    """Graph Attention Network"""
+    def __init__(self, dim_in, dim_h, dim_out, heads=2):
+        super().__init__()
+        self.gat1 = GATv2Conv(dim_in, dim_h, heads=heads)
+        self.gat2 = GATv2Conv(dim_h*heads, dim_out, heads=1)
+        self.lin = nn.Linear(dim_out, 2)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        h = F.dropout(x, p=0.6, training=self.training)
+        h = self.gat1(h, edge_index)
+        h = F.elu(h)
+        h = F.dropout(h, p=0.6, training=self.training)
+        h = F.elu(self.gat2(h, edge_index))
+        h = global_mean_pool(h, data.batch)
+        h = self.lin(h)
+        return h
 
