33046ba7f08b2f3a47e0ee50c40d1226ce9ccb4e
Ирина Князева <study.knyazeva@yandex.com>
2023-12-11 13:14:59 +0300

    fix

--- model/dataset.py
+++ model/dataset.py
@@ -9,10 +9,11 @@ from scipy.io import loadmat
 
 from torch_geometric.nn import knn_graph
 
+
 class OpenCloseDataset(Dataset):
     def __init__(self, datafolder, open_file, close_file, 
                  reload=False, test=False, transform=None, 
-                 pre_transform=None, k_degree=10, threshold=0.5, edge_attr=None):
+                 pre_transform=None, k_degree=10, edge_attr=None):
 
         self.reload = reload
         self.test = test
@@ -21,9 +22,7 @@ class OpenCloseDataset(Dataset):
         self.open  = open_file
         self.edge_attr = edge_attr
         self.k_degree = k_degree
-        self.threshold = threshold
-        #self.outliers = np.array([256, 257, 258, 259]) 
-        # [52, 256, 53, 257, 54, 258, 55, 259]           
+        
 
         if self.reload:
             for root, dirs, files in os.walk(f'{self.datafolder}/processed'):
@@ -45,6 +44,7 @@ class OpenCloseDataset(Dataset):
     def len(self):
         return len(self.close) + len(self.open)
     
+    
     def process(self):
 
         for index in range(len(self.open)):
@@ -53,41 +53,33 @@ class OpenCloseDataset(Dataset):
         for index in range(len(self.close)):
             _ = self._load_and_save(self.close[index], index, 'close')
 
-    def _load_and_save(self, matr, index, state):
 
-        #matr = np.delete(matr, self.outliers, 0)
-        #matr = np.delete(matr, self.outliers, 1)
+    def _load_and_save(self, matr, index, state):
 
         x = torch.from_numpy(matr).float()
 
-        if self.k_degree is not None:
-            #adj = self.compute_KNN_graph(matr, k_degree=self.k_degree)
-            #adj = torch.from_numpy(adj).float()
-            #edge_index, edge_attr = dense_to_sparse(adj)
-            #self.edge_attr = edge_attr
-            m = torch.from_numpy(matr).float()
-            edge_index = knn_graph(m, self.k_degree)
-        else:
-            adj = self._adjacency_threshold(x)
-            edge_index, edge_attr = dense_to_sparse(adj)
-            self.edge_attr = edge_attr
+        adj = self.compute_KNN_graph(matr, k_degree=self.k_degree)
+        adj = torch.from_numpy(adj).float()
+        edge_index, edge_attr = dense_to_sparse(adj)
+        self.edge_attr = edge_attr
+
 
         label = torch.tensor(0 if state == 'close' else 1).long()
         data = Data(x=x, edge_index=edge_index, edge_attr=self.edge_attr, y=label)
+
         index = index + len(self.open) if state == 'close' else index
 
-        if self.test:
-            torch.save(data,
-                       os.path.join(self.processed_dir, 'test_'
-                                    f'data_{index}.pt'))
-        else:
-            torch.save(data,
-                       os.path.join(self.processed_dir,
-                                    f'data_{index}.pt'))
+
+        torch.save(data,
+                    os.path.join(self.processed_dir,
+                                f'data_{index}.pt'))
+        
         return data
     
     def compute_KNN_graph(self, matrix, k_degree):
-        """ Calculate the adjacency matrix from the connectivity matrix."""
+        """ Calculate the adjacency matrix from the connectivity matrix.
+            Adapted from: https://github.com/QKmeans0902/GCN_MDD_Classification
+        """
 
         matrix = np.abs(matrix)
         idx = np.argsort(-matrix)[:, 0:k_degree]
@@ -119,12 +111,6 @@ class OpenCloseDataset(Dataset):
         W = W - W.multiply(bigger) + W.T.multiply(bigger)
 
         return W.todense()
-
-    def _adjacency_threshold(self, matr):
-        if isinstance(self.threshold, list):
-            return (self.threshold[0] > matr) + (matr > self.threshold[1]) * matr
-        else:
-            return np.abs(matr > self.threshold) * matr #(np.abs(matr) > self.threshold) * matr
 
 
     def get(self, idx):
@@ -138,138 +124,4 @@ class OpenCloseDataset(Dataset):
             data = torch.load(os.path.join(self.processed_dir,
                                            f'data_{idx}.pt'))
         return data
-
-
-class oldOpenCloseDataset(Dataset):
-    def __init__(self, datafolder, reload=False, test=False, transform=None, pre_transform=None, k_degree=10):
-        self.reload = reload
-        self.test = test
-        self.datafolder = datafolder
-        self.close = loadmat(f'{datafolder}/raw/resultsROI_Condition001.mat')['Z']
-        self.open  = loadmat(f'{datafolder}/raw/resultsROI_Condition002.mat')['Z']
-        self.edge_attr = None
-        self.k_degree = k_degree
-        self.dl = np.array([]) # [52, 256, 53, 257, 54, 258, 55, 259] [256, 257, 258, 259]
-
-        super().__init__(root=datafolder, transform=transform, pre_transform=pre_transform)
-
-    @property
-    def raw_file_names(self):
-        return ['resultsROI_Condition001.mat', 'resultsROI_Condition002.mat']
-    
-    @property
-    def processed_file_names(self):
-        if self.reload:
-            return [f'_data_{i}.pt' for i in range(84*2)]
-        else:
-            return [f'data_{i}.pt' for i in range(84*2)]
-        
-    def len(self):
-        return 84 + 84
-    
-    def download(self):
-        print('yo')
-    
-    def process(self):
-
-        for index in range(84):
-            _ = self._load_and_save(index, 'open')
-
-        for index in range(84):
-            _ = self._load_and_save(index, 'close')
-
-    def _load_and_save(self, index, state):
-
-        if state == 'open':
-            matr = self.open[:, :, index]
-        elif state == 'close':
-            matr = self.close[:, :, index]
-
-        # todo fill with 0 or 1
-        np.fill_diagonal(matr, 1)
-        #matr = np.delete(matr, self.dl, 0)
-        #matr = np.delete(matr, self.dl, 1)
-
-        x = torch.from_numpy(matr).float()
-
-        if self.k_degree is not None:
-            adj = self.compute_KNN_graph(matr, k_degree=self.k_degree)
-            adj = torch.from_numpy(adj).float()
-            edge_index, edge_attr = dense_to_sparse(adj)
-            self.edge_attr = edge_attr
-        else:
-            edge_index = self._adjacency_threshold(x)
-
-        label = torch.tensor(0 if state == 'close' else 1).long()
-
-        data = Data(x=x, edge_index=edge_index, edge_attr=self.edge_attr, y=label)
-
-        index = index + 84 if state == 'close' else index
-        if self.test:
-            torch.save(data,
-                       os.path.join(self.processed_dir, 'test',
-                                    f'data_{index}.pt'))
-        else:
-            torch.save(data,
-                       os.path.join(self.processed_dir,
-                                    f'data_{index}.pt'))
-        return data
-    
-    def compute_KNN_graph(self, matrix, k_degree):
-        """ Calculate the adjacency matrix from the connectivity matrix."""
-
-        matrix = np.abs(matrix)
-        idx = np.argsort(-matrix)[:, 0:k_degree]
-        matrix.sort()
-        matrix = matrix[:, ::-1]
-        matrix = matrix[:, 0:k_degree]
-
-        A = self._adjacency(matrix, idx).astype(np.float32)
-
-        return A
-
-    def _adjacency(self, dist, idx):
-
-        m, k = dist.shape
-        assert m, k == idx.shape
-        assert dist.min() >= 0
-
-        # Weight matrix.
-        I = np.arange(0, m).repeat(k)
-        J = idx.reshape(m * k)
-        V = dist.reshape(m * k)
-        W = coo_matrix((V, (I, J)), shape=(m, m))
-
-        # No self-connections.
-        W.setdiag(0)
-
-        # Non-directed graph.
-        bigger = W.T > W
-        W = W - W.multiply(bigger) + W.T.multiply(bigger)
-
-        return W.todense()
-
-    def _adjacency_threshold(self, matr, threshold=0.5):
-        # todo optimize ???
-        # todo переделать порог
-        idx = []
-        for i in range(len(matr)):
-            for j in range(len(matr)):
-                if abs(matr[i, j]) > threshold:
-                    idx.append((i, j))
-
-        return torch.tensor(idx).long().t().contiguous()
-
-    def get(self, idx):
-        """ - Equivalent to __getitem__ in pytorch
-            - Is not needed for PyG's InMemoryDataset
-        """
-        if self.test:
-            data = torch.load(os.path.join(self.processed_dir,
-                                           f'data_test_{idx}.pt'))
-        else:
-            data = torch.load(os.path.join(self.processed_dir,
-                                           f'data_{idx}.pt'))
-        return data
-    
 
--- model/explain.py
+++ model/explain.py
@@ -5,34 +5,35 @@ import numpy as np
 from collections import defaultdict
 
 
-def model_forward(model, edge_mask, data):
+def model_forward(edge_mask, data, model):
     batch = torch.zeros(data.x.shape[0], dtype=int).to('cpu')
     out = model(data.x, data.edge_index, edge_mask, batch)
     return out
 
 
-def explain(method, data, target=0):
+def explain(method, data, model, target=0):
     data.to('cpu')
+    model.to('cpu')
     input_mask = torch.ones(data.edge_index.shape[1]).requires_grad_(True).to('cpu')
     if method == 'ig':
         ig = IntegratedGradients(model_forward)
         mask = ig.attribute(input_mask, target=target,
-                            additional_forward_args=(data,),
+                            additional_forward_args=(data, model),
                             internal_batch_size=data.edge_index.shape[1])
     elif method == 'saliency':
         saliency = Saliency(model_forward)
         mask = saliency.attribute(input_mask, target=target,
-                                  additional_forward_args=(data,))
+                                  additional_forward_args=(data, model))
     else:
         raise Exception('Unknown explanation method')
 
     edge_mask = np.abs(mask.cpu().detach().numpy())
     if edge_mask.max() > 0:  # avoid division by zero
         edge_mask = edge_mask / edge_mask.max()
-    return edge_mask
+    return mask.cpu().detach().numpy() #edge_mask
 
 
-def aggregate_edge_directions(edge_mask, data):
+def aggregate_edge_directions(dataset, edge_mask, data):
     edge_mask_dict = defaultdict(float)
     for val, u, v in list(zip(edge_mask, *data.edge_index)):
         u, v = u.item(), v.item()
@@ -40,7 +41,33 @@ def aggregate_edge_directions(edge_mask, data):
             u, v = v, u
         edge_mask_dict[(u, v)] += val
 
-    matr = np.zeros((422, 422))
+    matr = np.zeros(dataset[0].x.shape)
     for k in edge_mask_dict:
         matr[k[0], k[1]] = edge_mask_dict[k]
-    return matr
+
+    return matr, edge_mask_dict
+
+
+def salient_edges(dataset, model, label):
+    """
+    label: label of interest, int
+
+    returns np.array
+    """
+    close_labels = [i for i, en in enumerate(dataset) if en.y == label]
+    sal_matr = np.zeros(dataset[0].x.shape)
+
+    for i in close_labels:
+        edge_mask = explain(method='saliency',
+                            data=dataset[i],
+                            model=model,
+                            target=dataset[i].y)
+
+        salient_edges, edge_dict = aggregate_edge_directions(dataset,
+            edge_mask, dataset[i])
+        sal_matr += salient_edges > 0
+
+    return to_symmetrical(sal_matr / len(close_labels))
+
+def to_symmetrical(matr):
+    return np.where(matr, matr, matr.T)
